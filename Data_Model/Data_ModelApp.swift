cad166e29269b564d34212e65d42fae905678a1b
Sebastian <s.kloesel@gmx.de>
2022-01-23 14:33:37 +0100

    Initial Commit

---
+++ Data_Model/Data_ModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_ModelApp.swift
+//  Data_Model
+//
+//  Created by Sebastian Klösel on 23.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Data_ModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
